9160634359397f6d073b76049597af559672d714
Harikrishnan <harikrishnanpm991@yahoo.com>
2016-12-15 23:31:17 +0530

    bug fix

--- src/Home/Others/other.js
+++ src/Home/Others/other.js
@@ -21,4 +21,4 @@ class OtherDevices extends Component {
 }
 
 const mapStateToProps = (state) => state;
-export default connect(mapStateToProps)(OthereDevices);
+export default connect(mapStateToProps)(OtherDevices);
